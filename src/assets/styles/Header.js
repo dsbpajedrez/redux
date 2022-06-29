0fdc48fdcf1de5f3c2eb22f2993584deecf3ca8a
David Santiago Benjumea Perez <davidbenjumchess@gmail.com>
2022-06-29 11:12:22 -0500

    new: Header e imgages

--- src/assets/styles/Header.js
+++ src/assets/styles/Header.js
@@ -1,7 +1,46 @@
 import styled from "styled-components";
+export const OptionsProfile= styled.ul`
+    display:none;
+    list-style:none;
+    position:absolute;
+    text-align:left;
+    background-color:#8f57fd;
+    right:0;
+    top:100px;
+    border-radius:0 0 0 20px;
+    padding:0.5rem;
+    width:60px;
+
+`
 
 export const Encabezado = styled.div`
     grid-column:1/14;
     grid-row:1/3;
+    background-color:#8f57fd;
+    display:flex;
+    justify-content: space-between;
+    color:white;
+    padding:0.5rem;
+    position:relative;
+    height:100px;
+`
+export const ImgLogo = styled.img`
+    width:20rem;
+`
 
+export const UserProfile = styled.div`
+ 
+
+`
+
+export const MenuProfile = styled.div`
+&:hover ${OptionsProfile}{        
+    display:block;        
+}
+    text-align:left;
+  
+`
+export const Item = styled.li`
+    width:100%;
+    text-align:left;
 `
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,21 @@
 import React from 'react'
-import { Encabezado } from '../assets/styles/Header'
+import { Encabezado, ImgLogo, Item, MenuProfile, OptionsProfile, UserProfile } from '../assets/styles/Header'
+import Logo from '../assets/static/logo-platzi-video-BW2.png'
+import userIcon from '../assets/static/user-icon.png'
 const Header = () => {
   return (
     <Encabezado>
-        <img/>
-        <div>
-            <img/>
-            <p>Perfil</p>
-        </div>
-        <ul>
-            <li><a>Cuenta</a></li>
-            <li><a>Cerrar</a></li>
-        </ul>
+        <ImgLogo src={Logo}/>  
+        <MenuProfile>
+          <UserProfile>
+            <img src={userIcon}/>
+              <p>Perfil</p>
+          </UserProfile>
+          <OptionsProfile>
+              <Item>Cuenta</Item>
+              <Item>Cerrar</Item>
+          </OptionsProfile>
+        </MenuProfile>   
     </Encabezado>
   )
 }
